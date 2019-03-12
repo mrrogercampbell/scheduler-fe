04e59fb1e4187a7b6a56a286018eac946dd4b936
Roger Campbell II <rc1336@gmail.com>
2019-03-12 19:32:02 -0400

    Minor css changes made to WeeklyAv component

--- src/Components/WeeklyAvailability/WeeklyAvailability.js
+++ src/Components/WeeklyAvailability/WeeklyAvailability.js
@@ -22,7 +22,7 @@ class WeeklyAvailability extends Component {
             // const obj = JSON.parse(item.mon_am)
             // console.log(obj)
             return (
-                <div key={item.id} weekly-availability-container>
+                <div key={item.id} className='weekly-availability-container'>
                     <p>Employee Number: {item.employee}</p>
                     <ul>
                         <li>Mon AM: <span className='bold-span'> {item.mon_am}</span></li>
@@ -52,7 +52,7 @@ class WeeklyAvailability extends Component {
         })
         return (
 
-            <div className='employee-container'>
+            <div>
                 {weeklyAvailability}
                 {/* <ul><li>Mon AM: {obj.mon_am}</li></ul> */}
                 <h2>Hello from weeklyAvailability component</h2>
