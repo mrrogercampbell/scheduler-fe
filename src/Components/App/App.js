eb3e5220f06fbbe6289bd852b5162003e728511b
Roger Campbell II <rc1336@gmail.com>
2019-03-12 19:30:02 -0400

    Was able to get the WeeklyAvailability Component to render properly

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Employee from '../Employee/Employee';
+import WeeklyAvailability from '../WeeklyAvailability/WeeklyAvailability';
 
 class App extends Component {
 
@@ -7,6 +8,7 @@ class App extends Component {
     return (
       <div>
         <Employee />
+        <WeeklyAvailability />
       </div>
     );
   }
