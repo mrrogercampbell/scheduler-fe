4ae5ecfbab1cfa83d4a4376372be8bebb79777f5
Courtney Tyler <courtneytyler7@gmail.com>
2019-03-13 19:19:13 -0400

    attempt to fix 403 and add code for shiftsched component

--- src/Components/ShiftSched/ShiftSchedCreate.js
+++ src/Components/ShiftSched/ShiftSchedCreate.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 
 class ShiftSchedCreate extends Component {
     constructor() {
@@ -34,9 +35,59 @@ class ShiftSchedCreate extends Component {
     render() {
         const { date, shift, num_of_sections, section_red, section_orange, section_yellow, section_green, section_blue, section_purple } = this.state;
         return (
-            <div>
-                
-            </div>
+            <form onSubmit={this.onSubmit}>
+            <label>Date:</label>
+            <input
+              type="date"
+              name="date"
+              value={date}
+              onChange={this.onChange}
+            />
+            <br />
+            <label>Shift:</label>
+            <select value={shift} onChange={this.onChange}>
+                <option name='shift' value='AM'>AM Shift</option>
+                <option name='shift' value='AFT'>Afternoon Shift</option>
+                <option name='shift' value='PM'>PM Shift</option>
+            <br />
+            <label>Number of Sections</label>
+            <input
+              type="number"
+              name="num_of_sections"
+              value={num_of_sections}
+              onChange={this.onChange}
+            />
+            <br />
+            <label>Section Red: </label>
+            <select value={section_red} onChange={this.onChange}>
+                <option name='section_red' value='Employee'>Manager</option>
+            </select>
+            <label>Logo URL:</label>
+            <input
+              type="text"
+              name="logoURL"
+              value={logoURL}
+              onChange={this.onChange}
+            />
+            <br />
+            <label>Content:</label>
+            <input
+              type="text"
+              name="content"
+              value={content}
+              onChange={this.onChange}
+            />
+            <br />
+            <label>Location:</label>
+            <input
+              type="text"
+              name="location"
+              value={location}
+              onChange={this.onChange}
+            />
+            <br />
+            <button type="submit">Submit</button>
+          </form>
         );
     }
 }
