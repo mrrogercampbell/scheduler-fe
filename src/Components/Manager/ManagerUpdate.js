1f7460a4d7053f8a7677ad6f6a79d6626f9c0806
Roger Campbell II <rc1336@gmail.com>
2019-03-14 10:00:13 -0400

    Updated ManagerUpdate component's options. It is now able to successfully update information.

--- src/Components/Manager/ManagerUpdate.js
+++ src/Components/Manager/ManagerUpdate.js
@@ -31,46 +31,46 @@ class ManagerUpdate extends Component {
         this.setState({ manager: state });
     }
 
-    onSubmit =(e)=> {
+    onSubmit = (e) => {
         console.log("update submitting")
         e.preventDefault();
         const { full_name, position, photo_url } = this.state.manager
         // put request throwing 403 - forbidden
         axios.put('http://localhost:8000/api/manager/' + this.props.match.params.id, { full_name, position, photo_url })
-        .then((res) => {
-            console.log(res)
-            this.props.history.push('/manager/' + this.props.match.params.id)
-        });
-        
+            .then((res) => {
+                console.log(res)
+                this.props.history.push('/manager/' + this.props.match.params.id)
+            });
+
     }
 
     render() {
         const { full_name, position, photo_url } = this.state.manager
         return (
-            
+
             <form onSubmit={this.onSubmit}>
                 <h3>Edit Manager Details</h3>
                 <label>Full Name:</label>
                 <input
-                type="text"
-                name="full_name"
-                value={full_name}
-                onChange={this.onChange}
+                    type="text"
+                    name="full_name"
+                    value={full_name}
+                    onChange={this.onChange}
                 />
                 <br />
                 <label>Position: </label>
                 <select value={position} onChange={this.onChange}>
-                    <option name='position' value='Manager'>Manager</option>
-                    <option name='position' value='General Manager'>General Manager</option>
-                    <option name='position' value="Assistant Manager">Assistant Manager</option>
+                    <option name='position' value='manager'>Manager</option>
+                    <option name='position' value='general_manager'>General Manager</option>
+                    <option name='position' value="assistant_manager">Assistant Manager</option>
                 </select>
                 <br />
                 <label>Photo URL:</label>
                 <input
-                type="text"
-                name="photo_url"
-                value={photo_url}
-                onChange={this.onChange}
+                    type="text"
+                    name="photo_url"
+                    value={photo_url}
+                    onChange={this.onChange}
                 />
                 <button type="submit">Submit</button>
             </form>
