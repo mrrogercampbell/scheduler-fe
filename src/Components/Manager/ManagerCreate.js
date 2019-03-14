5963fda3eea689c1055d8a27a736113df4f00b31
Roger Campbell II <rc1336@gmail.com>
2019-03-13 21:23:25 -0400

    Have drop down working now to get it to pass the value to state.

--- src/Components/Manager/ManagerCreate.js
+++ src/Components/Manager/ManagerCreate.js
@@ -2,59 +2,59 @@ import React, { Component } from 'react';
 import axios from "axios";
 
 class ManagerCreate extends Component {
-    constructor() {
-        super();
-        this.state = {
-        full_name: "",
-        // position: "",
-        photo_url: ""
-        };
-        this.onChange = this.onChange.bind(this);
-        this.onSubmit = this.onSubmit.bind(this);
-    }
-
-    onChange = e => {
-        this.setState({ [e.target.name]: e.target.value });
+  constructor() {
+    super();
+    this.state = {
+      full_name: "",
+      position: "",
+      photo_url: ""
     };
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
 
-    onSubmit = e => {
-        e.preventDefault();
-        axios.post('http://localhost:8000/api/managers', this.state).then(result => {
-          console.log(result)
-          this.props.history.push("/managers");
-        });
-      };
+  onChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
 
-    render() {
-        const { full_name, position, photo_url } = this.state;
-        return (
-            <form onSubmit={this.onSubmit}>
-            <label>Full Name:</label>
-            <input
-              type="text"
-              name="full_name"
-              value={full_name}
-              onChange={this.onChange}
-            />
-            <br />
-            <label>Position: </label>
-            <select value={position} onChange={this.onChange}>
-                <option name='position' value='Manager'>Manager</option>
-                <option name='position' value='General Manager'>General Manager</option>
-                <option name='position' value="Assistant Manager">Assistant Manager</option>
-            </select>
-            <br />
-            <label>Photo URL:</label>
-            <input
-              type="text"
-              name="photo_url"
-              value={photo_url}
-              onChange={this.onChange}
-            />
-            <button type="submit">Submit</button>
-          </form>
-        );
-    }
+  onSubmit = e => {
+    e.preventDefault();
+    axios.post('http://localhost:8000/api/managers', this.state).then(result => {
+      console.log(result)
+      this.props.history.push("/managers");
+    });
+  };
+
+  render() {
+    const { full_name, position, photo_url } = this.state;
+    return (
+      <form onSubmit={this.onSubmit}>
+        <label>Full Name:</label>
+        <input
+          type="text"
+          name="full_name"
+          value={full_name}
+          onChange={this.onChange}
+        />
+        <br />
+        <label>Position: </label>
+        <select value={this.state.position.value} onChange={this.onChange}>
+          <option name='position' value='Manager'>Manager</option>
+          <option name='position' value='General Manager'>General Manager</option>
+          <option name='position' value="Assistant Manager">Assistant Manager</option>
+        </select>
+        <br />
+        <label>Photo URL:</label>
+        <input
+          type="text"
+          name="photo_url"
+          value={photo_url}
+          onChange={this.onChange}
+        />
+        <button type="submit">Submit</button>
+      </form>
+    );
+  }
 }
 
 export default ManagerCreate;
